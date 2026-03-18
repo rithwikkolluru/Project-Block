50d382319415b2891e40b07d7cee0152e13313d8
AIML learner <rithwikkolluru7@gmail.com>
2026-03-18 11:37:32 +0530

    fix(deps): update lenis, replace deprecated @studio-freight/lenis

--- src/GravityProvider.tsx
+++ src/GravityProvider.tsx
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 
 export const GravityProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   useLayoutEffect(() => {
